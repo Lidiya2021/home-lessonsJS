bf5a8ed4ee16b753e8ad92fa79fa8fa9692c0efe
Lidiya2021 <suliz2009@rambler.ru>
2021-10-07 20:43:09 +0300

    добавлен метод подсчета суммы товара на странице

--- script.js
+++ script.js
@@ -31,47 +31,58 @@
 
 //Создали класс для карточки товара
 class GoodsItem {
-  constructor(img, title, price) {
-    this.img = img;
-    this.title = title;
-    this.price = price;
-  }
-  //этим метоом вернули НТМL разметку
-  render() {
-    return `
+    constructor(title, price) {
+            this.title = title;
+            this.price = price;
+        }
+        //этим метоlдом вернули НТМL разметку
+    render() {
+        return `
     <div class="goods-item">
-      <img src="${this.img}" alt="карточка товара">
       <h3>${this.title}</h3>
       <p>${this.price}</p>
       <button class="button3">Добавить в корзину<button>
     </div>
   `;
-  }
+    }
 }
 //создали класс для списка товаров
 class GoodsList {
-  constructor() {
-    this.goods = [];
-  }
-  //этим методом заполнили список товаров
-  fetchGoods() {
-    this.goods = [
-      { img: 'img//product1.png', title: 'Shirt', price: 150 },
-      { img: 'img//product2.png', title: 'Socks', price: 50 },
-      { img: 'img//product3.png', title: 'Jacket', price: 350 },
-      { img: 'img//product4.png', title: 'Shoes', price: 250 },
-      { img: 'img//product5.png', title: 'Jacket', price: 350 },
-      { img: 'img//product6.png', title: 'Shoes', price: 250 },
-    ];
-  }
-  render() {
-    let listHtml = '';
-    this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.img, good.title, good.price);
-      listHtml += goodItem.render();
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml;
-  }
+    constructor() {
+            this.goods = []; //список товаров
+        }
+        //метод, который обращался бы к серверу и получал данные
+    fetchGoods() {
+            this.goods = [
+                { title: 'Shirt', price: 150 },
+                { title: 'Socks', price: 50 },
+                { title: 'Jacket', price: 350 },
+                { title: 'Shoes', price: 250 },
+            ];
+        }
+        //создаем новый элемент
+    createCount() {
+            const h2Count = document.createElement('h2');
+            h2Count.classList.add('.count');
+            h2Count.innerHTML = `ИТОГО, общая стоимость товаров составляет: ${this.getCount()} рублей `;
+
+            document.body.appendChild(h2Count);
+        }
+        //метод для перебора массива товаров 
+    render() {
+            let listHtml = '';
+            this.goods.forEach(good => {
+                const goodItem = new GoodsItem(good.title, good.price);
+                listHtml += goodItem.render();
+            });
+            document.querySelector('.goods-list').innerHTML = listHtml;
+
+            this.createCount();
+        }
+        //Определение суммы товаров
+    getCount() {
+        return this.goods.reduce((prev, { price }, array) => prev + price, 0);
+    }
 }
 
 const list = new GoodsList();
