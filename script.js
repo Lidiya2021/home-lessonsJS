095478155d1bcb975bb0c48231bd2972dcf0892b
Lidiya2021 <suliz2009@rambler.ru>
2021-10-16 23:52:45 +0300

    переделываем магазин на VUE

--- script.js
+++ script.js
@@ -1,154 +1,76 @@
-/*function makeGETRequest(url, callback) {
-    var xhr;
-    
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-      } else if (window.ActiveXObject) { 
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-      }
-    
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          callback(xhr.responseText);
-        }
-      }
-    
-      xhr.open('GET', url, true);
-      xhr.send();
-  }*/
+const GOODS =  [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+]
 
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const CORE_API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const GET_GOODS_URL = "/catalogData.json";
+const GET_BASKET_GOODS_URL = "/getBasket.json ";
 
-// Функция запроса / ответа на промисах
-function makeGETRequest(url, callback) {
-    return new Promise((resolve, reject) => {
-        let xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject;
-        xhr.open("GET", url, true);
-        xhr.onload = () => resolve(callback(xhr.responseText));
-        xhr.onerror = () => reject(xhr.statusText);
+const service = (method, postfix) => (
+    new Promise((resolve) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, `${CORE_API_URL}${postfix}`, true);
         xhr.send();
-    });
-}
+        xhr.onload = (event) => {
+        resolve(JSON.parse(event.target.response));
+        }
+    })
+);
 
-class GoodsItem {
-    constructor(product_name, price) {
-        this.product_name = product_name;
-        this.price = price;
-    }
+Vue.component('basket-goods-item', {
+    props: [''],
+    template: `
+        <div class="basket-goods-item" 
+        <div>{{ item.title }}</div>
+        <div></div>
+        <div>{{ item.price }}</div>
+        </div>
+    `
+});
+    
+Vue.component('basket-card', {
+    props: [''],
+    template: `
+        <div class="basket-card">
+            <div></div>
+        </div>
+    `
+});
+  
 
-    render() {
-        return `
-    <div class="goods-item">
-      <h3>${this.product_name}</h3>
-      <p>${this.price}</p>
-      <button class="button3">Добавить в корзину<button>
-    </div>`;
-    }
-};
-
-class GoodsList {
-    constructor() {
-        this.goods = [];
-        this.filteredGoods = [];
-    }
-
-    fetchGoods(cb) {
-        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
-            this.goods = JSON.parse(goods);
-            this.filteredGoods = JSON.parse(goods);
-            cb();
-        })
-    };
-
-    render() {
-        let listHtml = '';
-        this.filteredGoods.forEach(good => {
-            const goodItem = new GoodsItem(good.product_name, good.price);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-
-    filterGoods(value) {
-        const regexp = new RegExp(value, 'i');
-        this.filteredGoods = this.goods.filter(goods => regexp.test(goods.product_name));
-        this.render();
-    }
-
-    /*searchButton.addEventListener('click', (e) => {
-        const value = searchInput.value;
-        list.filterGoods(value);
-    });*/
-}
-
-const list = new GoodsList();
-list.fetchGoods(() => {
-    list.render();
+Vue.component('goods-item', {
+    props: ['item'],
+    template: `
+        <div class="goods-item">
+           <div>{{ item.title }}</div>
+           <div>
+              {{ item.price }}
+           </div>
+           <div>
+              <custom-button>добавить</custom-button>
+           </div>
+        </div>
+    `,
 });
 
-class Basket {
-    constructor() {
-        this.element = document.createElement('button');
-        this.element.className = '.basket-card';
-        this.element.append('Корзина');
-    }
-    setClickHandler(clickHandler) {
-        this.element.onclick = clickHandler;
-    }
-    render() {
-        document.querySelector('header').append(this.element);
-    }
-};
+const app  = new Vue({
+    el: '#app',
+    data: {
+        goods: GOODS,
+        filteredGoods: GOODS,
+        basketCardVision: false,
+        search: '' 
+    },
+        
+    methods: {
+        filterGoods: function (event) {
+            this.filteredGoods = this.goods.filter(({ title }) => {
+                return new RegExp(this.search, 'i').test(title);
+            })
+        }
 
-class BasketCard {
-    basketGoods = [];
-    constructor() {
-        this.setVision.bind(this);
-        this.render.bind(this);
     }
-    fetchBasketGoods() {
-        returngetBasketGoods().then((basketGoods) => {
-            this.basketGoods = this.basketGoods.concatants;
-        })
-    }
-    setVision(vision) {
-        document.querySelector('basket-card').style.display = vision ? 'block' : 'none';
-    }
-    element = `
-    <div class = "basket-card">
-        <div class = 'basket-card__header'>
-            <button class = 'close-basket-card'> Закрыть 
-            </button> 
-        </div>
-        <div class = 'basket-card__body'>
-        </div>
-    </div>
-    `;
-    render() {
-        document.querySelector("body").insertAdjacentHTML("beforeend", this.element);
-        document.querySelector('.close-basket-card').onclick = this.setVision.bind(this, false)
-        document.querySelector('.basket-card__body').innerHTML = this.basketGoods.map(({ product_name, price }) => {
-            const _basketItem = new BasketItem(product_name, price);
-            return _basketItem.render();
-        }).join('')
-    }
-};
-
-class BasketItem {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-    render() {
-        return `
-            <div class='basket-item'>
-                <div>
-                    <h3>${this.title}</h3>
-                    <p>${this.price}</p>
-                </div>
-                <div>
-                <button>удалить</button>
-            </div>
-        `;
-    }
-};
+});
